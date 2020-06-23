98ff61857c401ffc1272e25fcd03c1b999608ceb
nzinfo <limn@coreseek.com>
2020-06-23 22:52:25 +0800

    addfonts

---
+++ Gemfile
@@ -0,0 +1,7 @@
+source 'https://rubygems.org'
+
+gem 'asciidoctor-pdf'
+gem 'rouge'
+gem 'prawn-gmagick'
+gem 'prawn', github: 'prawnpdf/prawn'
+gem 'prawn-table', github: 'prawnpdf/prawn-table'
---
+++ build.sh
@@ -0,0 +1,1 @@
+asciidoctor -r asciidoctor-diagram  -r asciidoctor-pdf -b pdf  -a scripts=cjk -a pdf-theme=./data/themes/KaiGenGothicCN-theme.yml -a pdf-fontsdir=./data/fonts doc/chapter_1/01_introduction.adoc
